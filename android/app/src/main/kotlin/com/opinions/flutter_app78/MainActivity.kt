288904f980dd262c2d58756f2ab755184a3ea3e6
Pieter <pieterventer571@gmail.com>
2021-02-13 16:40:54 +0200

    some basics with stateful widgets

---
+++ android/app/src/main/kotlin/com/opinions/flutter_app78/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.opinions.flutter_app78
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/my_home_page.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyHomePageState();
+  }
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  int _pictureNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("The Flutter Series: Part 2"),
+        centerTitle: true,
+      ),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          //aligns the children of the column to the center, vertically
+          children: [
+            Container(
+              width: double.infinity,
+              height: 500,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image:
+                        AssetImage("assets/images/image$_pictureNumber.jpg")),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  child: RaisedButton(
+                      child: Text("Previous"),
+                      onPressed: () {
+                        setState(() {
+                          _pictureNumber--;
+                        });
+                      }),
+                  margin: EdgeInsets.only(right: 8, top: 8),
+                ),
+                Container(
+                  child: RaisedButton(
+                      child: Text("Next"),
+                      onPressed: () {
+                        setState(() {
+                          _pictureNumber++;
+                        });
+                      }),
+                  margin: EdgeInsets.only(left: 8, top: 8),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
